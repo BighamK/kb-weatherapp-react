b84fb36303005bfa3957035a099c7501866c812c
BighamK <100013654+BighamK@users.noreply.github.com>
2022-06-23 09:49:53 -0700

    Setup basic info for Main Weather

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,24 @@ import "./Weather.css";
 
 export default function Weather() {
     return (
-        <div> WeAtHeR argh</div>
+        <div className="Weather">
+            <h1> Main Weather </h1>
+            <ul>
+                <li>Date and Time</li>
+                <li>Description</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img 
+                    src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy"/>50°F
+                </div>
+                <div className="col-6"></div>
+<ul>
+    <li>Precipitation</li>
+    <li>Humidity</li>
+    <li>Wind</li>
+</ul>
+            </div>
+            </div>
     )
 }
