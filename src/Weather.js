463c7f33a601b035e33ab6389d8fe21e567906e7
BighamK <100013654+BighamK@users.noreply.github.com>
2022-06-24 12:57:44 -0700

    Start entering real weather data

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+    const [ready, setReady] = useState(false);
+    const [weatherData, setWeatherData] = useState({});
+    function handleResponse(response) {
+        console.log(response.data);
+        setWeatherData({
+            temperature: response.data.main.temp,
+            description: response.data.weather[0].description,
+            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            wind: response.data.wind.speed,
+            humidity: response.data.main.humidity,
+            city: response.data.name
+        });
+
+        setReady(true);
+    }
+   
+    if (ready) {
     return (
         <div className="Weather">
             <h1>Weather Forecast</h1>
@@ -21,27 +38,35 @@ export default function Weather() {
                     </div>
                 </div>
             </form>
-            <h1> Current City </h1>
+            <h1>{weatherData.city}</h1>
             <ul>
                 <li>Date and Time</li>
-                <li>Description</li>
+                <li>{weatherData.description}</li>
             </ul>
             <div className="row">
                 <div className="col-6">
                     <img 
-                    src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy"
+                    src={weatherData.iconUrl} 
+                    alt={weatherData.description} 
                     />
-                    <span className="temperature">50</span>
+                    <span className="temperature">{Math.round(weatherData.main.temp)}</span>
                      <span className="unit">°F</span>
                 </div>
                 <div className="col-6">
                   <ul>
-                      <li>Precipitation</li>
-                      <li>Humidity</li>
-                      <li>Wind</li>
+                      <li>Precipitation: unknown</li>
+                      <li>Humidity: {weatherData.humidity} %</li>
+                      <li>Wind: {weatherData.wind} mph</li>
                  </ul>
                  </div>
             </div>
         </div>
-    )
+    );
+    } else {
+
+    const apiKey = "6a8171dcf4cbfa0f9704ad8a2b755aa0";
+    let city = "Portland";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}lat={lat}&lon={lon}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+    }
 }
