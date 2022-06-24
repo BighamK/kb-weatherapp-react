aa1d042c5dd99ee1a035cbc081c86671b173d5ce
BighamK <100013654+BighamK@users.noreply.github.com>
2022-06-24 15:56:49 -0700

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-    const [weatherData, setWeatherData] = useState({ ready: false });
+    const [weatherData, setWeatherData] = useState({ ready: true });
     
     function handleResponse(response) {
         setWeatherData({
