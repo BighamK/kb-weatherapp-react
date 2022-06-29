fda1e86d8c1835de8242fd88ccfc93f087decf08
BighamK <100013654+BighamK@users.noreply.github.com>
2022-06-29 16:05:37 -0700

    Still working on weekly icon size

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -26,7 +26,7 @@ function day() {
         <div className="Daily">{day()}
 
 </div>
-<img className="forecastIcon" src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`} alt={props.data.weather[0].description} />
+<img className="forecastIcons" src={`http://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`} alt={props.data.weather[0].description} />
 <div className="Daily-temps">
    <span className="Daily-temp-max">{maxTemperature()}</span>
     <span className="Daily-temp-min">| {minTemperature()}</span>
