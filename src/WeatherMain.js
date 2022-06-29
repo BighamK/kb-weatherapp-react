adf0d558c35caf834ab41b6297a03d2d6294e6e0
BighamK <100013654+BighamK@users.noreply.github.com>
2022-06-29 10:32:14 -0700

    Nope

--- src/WeatherMain.js
+++ src/WeatherMain.js
@@ -28,7 +28,7 @@ export default function WeatherMain (props) {
         </div>
         <div className="col-6">
           <ul>
-              <li>Precipitation: {props.data.rain}</li>
+              <li>Precipitation: </li>
               <li>Humidity: {props.data.humidity} %</li>
               <li>Wind: {props.data.wind} mph</li>
          </ul>
